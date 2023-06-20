d475dfe2829a501cc64d3e12ffe15ebe266b6b6c
FredericoGrz <frederico.graz@gmail.com>
2023-06-20 10:34:51 -0500

    Commit Final

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,6 @@
+const { hash, compare } = require("bcryptjs")
 const AppError = require("../utils/AppError")
+const sqliteConnection = require("../database/sqlite")
 
 class UsersController {
   /**
@@ -8,14 +10,60 @@ class UsersController {
    * update - PUT para atualizar um registro
    * delete - DELETE para remover um registro
    */
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body
 
-    if(!name){
-      throw new AppError("Nome é obrigatorio")
+    const database = await sqliteConnection()
+    const checkUserExist = await database.get(`SELECT * FROM users WHERE email = '${email}'`)
+    if (checkUserExist)
+      throw new AppError("Este email ja esta em uso", 409)
+
+    const hashedPassword = await hash(password, 8)
+
+    await database.run(`INSERT INTO users (name, email, password) VALUES ('${name}', '${email}', '${hashedPassword}')`)
+
+    return response.status(201).json()
+  }
+
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body
+    const { id } = request.params
+
+    const database = await sqliteConnection()
+
+    const user = await database.get(`SELECT * FROM users WHERE id = '${id}'`)
+
+    if (!user)
+      throw new AppError("Usuario nao encontrado", 404)
+
+    const userWithUpdatedEmail = await database.get(`SELECT * FROM users where email = '${email}'`)
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id)
+      throw new AppError("Este email já esta em uso")
+
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    if (password && !old_password)
+      throw new AppError("Voce precisa informar a senha antiga para definir a nova senha")
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if (!checkOldPassword)
+        throw new AppError("A senha antiga nao confere")
+
+      user.password = await hash(password, 8)
     }
 
-    response.status(201).json({ name, email, password })
+    await database.run(`UPDATE users SET
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = DATETIME('now')
+      WHERE id = ?`, [user.name, user.email, user.password, id])
+
+    return response.status(200).json()
   }
 
 }
--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 require("express-async-errors")
 
+const migrationsRun = require("./database/sqlite/migrations")
+
 const AppError = require("./utils/AppError")
 
 const express = require("express");
 
 //Por padrao quando a gente nao diz o arquivo de uma pasta, ele carrega o arquivo index
 const routes = require("./routes")
+migrationsRun()
 
 const app = express();
 
@@ -14,6 +17,7 @@ const PORT = 3333;
 //Diz pra aplicacao que o padrao de request vira em formato de json
 app.use(express.json())
 app.use(routes)
+
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({
